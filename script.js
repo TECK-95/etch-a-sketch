064a74ade2d924c8f489830cd234afd325c6fa9f
Thomas Eckersley <ecke2225@mylaurier.ca>
2023-09-24 17:41:48 -0400

    Created loop to generate grid based on dimensions

---
+++ script.js
@@ -0,0 +1,13 @@
+const grid = document.querySelector(".grid");
+
+let dimensions = 16;
+let boxes = dimensions * dimensions;
+let gridSize = 500;
+
+for (i=1;i<=boxes;i++) {
+    const box = document.createElement("div");
+    box.classList.add("box");
+    box.style.height = gridSize/dimensions + "px";
+    box.style.width = gridSize/dimensions + "px";
+    grid.appendChild(box);
+}
