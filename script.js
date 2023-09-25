364c3b548208be1aeb7c1239ec8b865d586f9596
Thomas Eckersley <ecke2225@mylaurier.ca>
2023-09-25 18:08:31 -0400

    Set function to add new boxes
    Error with boxes appearing under old grid

--- script.js
+++ script.js
@@ -12,17 +12,35 @@ for (i=1;i<=boxes;i++) {
     grid.appendChild(box);
 }
 
+const newGrid = document.querySelector(".newGrid");
+function resetGrid() {
+    let newGridSize = prompt("Choose a number up to 100 for a new grid:");
+    if (newGridSize > 100) {
+        alert("Please choose a valid number");
+        return
+    }
+    let dimensions = newGridSize;
+    let boxes = dimensions * dimensions;
+    let gridSize = 500;
+    for (i=1;i<=boxes;i++) {
+        const box = document.createElement("div");
+        box.classList.add("box");
+        box.style.height = gridSize/dimensions + "px";
+        box.style.width = gridSize/dimensions + "px";
+        grid.appendChild(box);
+    }
+    const gridBoxes = document.querySelectorAll(".box");
+    gridBoxes.forEach((box) => {
+        box.addEventListener("mouseover", function(e) {
+            e.target.style.backgroundColor = "black";
+        })
+    })
+}
+
+newGrid.addEventListener("click", resetGrid);
 const gridBoxes = document.querySelectorAll(".box");
 gridBoxes.forEach((box) => {
     box.addEventListener("mouseover", function(e) {
         e.target.style.backgroundColor = "black";
     })
 })
-
-const newGrid = document.querySelector(".new");
-function resetGrid() {
-    let newGridSize = parseInt(prompt("Choose a number up to 100 for a new grid:"));
-    if (newGridSize > 100) {
-        alert("Please choose a valid number");
-    }
-}
